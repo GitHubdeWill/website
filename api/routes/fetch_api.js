3a8389d10d8a474a7cef13a9d021bb176a6a7c0d
GitHubdeWill <hd209458inj@gmail.com>
2020-05-25 01:57:33 -0400

    added express backend

---
+++ api/routes/fetch_api.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require('path');
+
+var router = express.Router();
+
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
+
+var list_dir = async function list_dir (dir_name) {
+    const file_list = await exec('ls -l '+dir_name+" | sed 1d | awk '{print $9} {print $6, $7, $8}'");
+    return file_list;
+};
+
+router.get("/list_posts", async function(req, res, next) {
+    var post_dir =  path.join(__dirname, '../public/markdowns');
+    var ret = await list_dir(post_dir);
+    var retjson = '{"posts":['+JSON.stringify(ret.stdout.trim()).replace(/\\n/g, '","')+']}';
+    console.log(retjson);
+
+    res.setHeader('Content-Type',"application/json");
+    res.send(retjson);
+});
+
+module.exports = router;
