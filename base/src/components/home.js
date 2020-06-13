41b81b65740e7d5ba3874b1b2a1cfaf9f716588c
GitHubdeWill <hd209458inj@gmail.com>
2020-06-13 08:19:45 -0400

    added more components

--- base/src/components/home.js
+++ base/src/components/home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import HomeCarousel from '../shared_components/carousel'
+
 class Home extends Component{
    render(){
       return(
          <div>
+            <HomeCarousel/>
             <h1>Home</h1>
          </div>
       );
